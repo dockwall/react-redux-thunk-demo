0efae0f0844bd9ae2f5fa1dbf3792beacf585c91
Stas <leshuksu@gmail.com>
2022-11-29 21:06:10 +0300

    Created reducers, App component

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+
+import App from "./components/App";
+import reducers from './reducers'
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+    <Provider store={createStore(reducers)}>
+        <App />
+    </Provider>
+);
