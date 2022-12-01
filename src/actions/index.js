af0f43b75ac97b226117545734aebb74d2fc151d
Stas <leshuksu@gmail.com>
2022-12-01 20:23:18 +0300

    Played with redux-thunk, used it and axios. Action is done

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,18 @@
-export const fetchPosts = () => {
-    return {
-        type: 'FETCH_POSTS'
-    }
+import jsonPlaceholder from "../api/jsonPlaceholder";
+
+// How to use redux-thunk as a middleware?
+// Thunk allows us to return objects AND functions from Action Creators.
+// In "function", we can STOP, MODIFY or do something else with action.
+// If we want to dispatch action, we need it to do MANUALLY (first argument)
+// Also we can use second argument - getStore/ It get us all current data from Redux store
+
+// fn => fn => ... - one function returns other function that returns ...
+
+export const fetchPosts = () => async dispatch => {
+    const response = await jsonPlaceholder.get('/posts');
+
+    dispatch({
+        type: 'FETCH_POSTS',
+        payload: response
+    });
 };
--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
 
 import App from "./components/App";
 import reducers from './reducers'
 
+// We import "applyMiddleware" from Redux.
+// It allows us to connect redux-thunk as middleware to Redux
+
+const store = createStore(reducers, applyMiddleware(thunk));
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-    <Provider store={createStore(reducers)}>
+    <Provider store={store}>
         <App />
     </Provider>
 );
